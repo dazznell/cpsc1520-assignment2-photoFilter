20fcb7ae7cdb6234922d62ff9beb4bf107ca9263
dazznell <dazznell@users.noreply.github.com>
2020-11-14 20:18:25 -0700

    init

--- js/main.js
+++ js/main.js
@@ -6,18 +6,6 @@ window.addEventListener('load', function(e){
         // console.log(photoArray)
         // console.log(resetButton)
 
-        resetButton.addEventListener('click', onNewFilter)
-
-        function onNewFilter(e){
-            const newFilter = tagFilter.value.trim();
-
-            if(newTagFilter !== ""){
-
-            }else{
-
-            }
-        } //END OF EVENT HANDLER
-
         tagFilter.addEventListener('input', onSearchPhotos)
 
         function onSearchPhotos(e){
@@ -46,20 +34,20 @@ window.addEventListener('load', function(e){
                     if(photoTag.includes(inputtedTag)){
                         // console.log("THIS IS THE ONE!!!!")
                         photo.classList.add('thumb-display');
-                        if(photo.classList.contains('thumb-display hidden')){
-                            photo.classList.remove('thumb-display hidden')
+                        if(photo.classList.contains('thumb-display')){
+                            photo.classList.remove('thumb-display')
                         }
                     }else{
                         // console.log("HIDE THIS STUFF!!!")
-                        photo.classList.add('hidden')
+                        photo.classList.add('thumb-display')
                     }
             }) // END OF SEARCHING
                 
             function resetDisplay(){
                 photoArray.forEach(function(photo, index){
 
-                    if(photo.classList.contains('thumb-display hidden')){
-                        photo.classList.remove('thumb-display hidden')
+                    if(photo.classList.contains('thumb-display')){
+                        photo.classList.remove('thumb-display')
                     }
                 })
             }
