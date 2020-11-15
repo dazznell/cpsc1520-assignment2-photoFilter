7e6b896140ee7d1b56028148a51eeb84efa22e30
dazznell <dazznell@users.noreply.github.com>
2020-11-14 18:07:38 -0700

    init

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Enter your JavaScript for the solution here...
+
+
+window.addEventListener('load', function(e){
+    // console.log(e)
+    const tagFilter = document.querySelector('#filter')
+    const photoArray = document.querySelectorAll('.tags')
+        console.log(photoArray)
+})
